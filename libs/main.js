d9be8ad09e61e467b2430d0755780d610f3f9404
Tomoya Koyanagi <tomk79@gmail.com>
2016-03-05 01:09:08 +0900

    一旦、なんとなくHTMLが出力されるところまで。

--- libs/main.js
+++ libs/main.js
@@ -1,8 +1,35 @@
 /**
  * express-pickles2.js
  */
-module.exports = function(){
+module.exports = function(execute_php, options){
+	// console.log(execute_php);
+	var px2proj = require('px2agent').createProject(execute_php);
+	// console.log(px2proj);
+
 	return function(req, res, next){
-		res.send('hello world');
+		// console.log(req);
+		console.log(req.originalUrl);
+
+		px2proj.get_config(function(value){
+			// console.log(value);
+
+			px2proj.query(req.originalUrl, {
+				"output": "json",
+				"userAgent": "Mozilla/5.0",
+				"success": function(data){
+					// console.log(data);
+				},
+				"complete": function(data, code){
+					// console.log(data, code);
+					var bin = data;
+					try {
+						data = JSON.parse(data);
+						bin = new Buffer(data.body_base64, 'base64').toString();
+					}catch(e){
+					}
+					res.send(bin);
+				}
+			});
+		});
 	};
 }
--- tests/mainTest.js
+++ tests/mainTest.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var express = require('express');
 var expressPickles2 = require('../libs/main.js');
+var http = require('http');
 
 describe('mainTest', function() {
 
@@ -8,13 +9,47 @@ describe('mainTest', function() {
 		this.timeout(60*1000);
 
 		var app = express();
-		app.get('/', expressPickles2() );
+		app.get('/*', expressPickles2(__dirname+'/htdocs/.px_execute.php') );
 		app.listen(3000);
 
-		setTimeout(function(){
-			assert.equal(1, 1);
-			done();
-		}, 10000);
+		get('/index.html', function(bin){
+			console.log(bin);
+
+			get('/index.html?PX=clearcache', function(bin){
+				console.log(bin);
+				setTimeout(function(){
+					assert.equal(1, 1);
+					done();
+				}, 5000);
+			});
+		});
+
 	});
 
 });
+
+function get(url, callback){
+
+	// ダウンロードする
+	var bin = '';
+	var req = http.get('http://127.0.0.1:3000'+url, function (res) {
+
+		// テキストファイルの場合は、エンコード指定は重要！
+		res.setEncoding('utf8');
+
+		// データを受け取るたびに、文字列を追加
+		res.on('data', function (data) {
+			bin += data;
+		});
+
+		// ファイルのダウンロードが終わるとendイベントが呼ばれる
+		res.on('end', function () {
+			callback(bin);
+		});
+	});
+
+	// 通信エラーなどはここで処理する
+	req.on('error', function (err) {
+		console.log('Error: ', err); return;
+	});
+}
